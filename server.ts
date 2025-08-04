915c84321f9928b1243b6e6a962fcc651d197461
Jonathan DIki <jonathandiki@outlook.com>
2025-08-04 17:38:56 +0200

    add express

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import { Request, Response } from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+});
